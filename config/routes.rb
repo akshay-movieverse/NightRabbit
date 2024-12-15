4f8d0d60e903bac4ef806b8374f2916e89ca8fb7
Rishabhjain072001 <rishabhjain072001@gmail.com>
2024-12-15 10:43:32 +0530

    Path Fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:registrations], controllers: { sessions: 'users/sessions' } # Disable signup routes
+  devise_for :users, skip: [:registrations], controllers: { sessions: 'users/sessions' }, path_names: { sign_in: 'sign_in', sign_out: 'sign_out' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resources :videos
+    resources :videos, only: [:index, :show]
   end
 end
