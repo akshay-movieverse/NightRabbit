3f30b6d009cae3dcdd61c82f4b3a3c9529ceca52
Rishabhjain072001 <rishabhjain072001@gmail.com>
2024-12-27 00:05:13 +0530

    Added video trailer

--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import '../styles/VideoCard.css';
 
 const VideoCard = ({ video }) => {
+  const [isHovered, setIsHovered] = useState(false);
   const navigate = useNavigate();
 
   const handleVideoClick = (videoId) => {
@@ -11,7 +12,18 @@ const VideoCard = ({ video }) => {
   };
 
   return (
-    <div className="video-card" onClick={()=>{handleVideoClick(video.id)}}>
+    <div
+      className="video-card"
+      onClick={() => handleVideoClick(video.id)}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {isHovered && video.metadata["trailerURL"] && (
+        <video className="video-trailer" autoPlay muted loop>
+          <source src={video.metadata["trailerURL"]} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      )}
       <img src={video.image_url} alt={video.title} className="video-thumbnail" />
       <h4 className="video-title">{video.title}</h4>
     </div>
