acd595521c0106f1acbd0293696a183e352fbbff
akshay-movieverse <67101074+akshay-movieverse@users.noreply.github.com>
2024-12-29 13:40:20 +0530

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,33 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_user!
 
-  # def authenticate_user!
-  #   token = request.headers['Authorization']&.split(' ')&.last  # Extract the token from the header
-
-  #   if token.nil? || decoded_token.nil?
-  #     render json: { error: 'Not Authenticated' }, status: :unauthorized
-  #   else
-  #     @current_user = User.find_by(id: decoded_token['user_id'], jti: decoded_token[:jti])
-  #     render json: {error: "Your login credentials are expires please contact admin for new login credentials"} if @current_user.expired?
-  #     render json: { error: 'Invalid token' }, status: :unauthorized if @current_user.nil?
-  #   end
-  # end
-
   def authenticate_user!
-    token = request.headers['Authorization']&.split(' ')&.last
-    Rails.logger.info "Token: #{token}"
-    if token.blank? || decoded_token.blank?
-      render_unauthorized('Not Authenticated')
+    token = request.headers['Authorization']&.split(' ')&.last  # Extract the token from the header
+
+    if token.nil? || decoded_token.nil?
+      render json: { error: 'Not Authenticated' }, status: :unauthorized
     else
       @current_user = User.find_by(id: decoded_token['user_id'], jti: decoded_token[:jti])
-      
-      if @current_user.nil?
-        render_unauthorized('Invalid token')
-      elsif @current_user.expired?
-        render_unauthorized('Your login credentials have expired. Please contact admin for new credentials.')
-      end
+      render json: {error: "Your login credentials are expires please contact admin for new login credentials"} if @current_user.expired?
+      render json: { error: 'Invalid token' }, status: :unauthorized if @current_user.nil?
     end
   end
+
+  # def authenticate_user!
+  #   token = request.headers['Authorization']&.split(' ')&.last
+  #   Rails.logger.info "Token: #{token}"
+  #   if token.blank? || decoded_token.blank?
+  #     render_unauthorized('Not Authenticated')
+  #   else
+  #     @current_user = User.find_by(id: decoded_token['user_id'], jti: decoded_token[:jti])
+      
+  #     if @current_user.nil?
+  #       render_unauthorized('Invalid token')
+  #     elsif @current_user.expired?
+  #       render_unauthorized('Your login credentials have expired. Please contact admin for new credentials.')
+  #     end
+  #   end
+  # end
 
   private
 
